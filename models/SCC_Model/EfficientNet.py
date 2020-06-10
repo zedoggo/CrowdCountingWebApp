c37cec3d5ef284b3ac94ac3981c825e608a1cfbe
Rivaldi Gunarso <gunarsorivaldi@gmail.com>
2020-06-10 16:50:05 +0700

    EfficientNet

---
+++ models/SCC_Model/EfficientNet.py
@@ -0,0 +1,47 @@
+import torch.nn as nn
+import torch
+from torchvision import models
+
+# from misc.layer import convDU, convLR
+
+import torch.nn.functional as F
+from misc.utils import *
+
+import pdb
+
+from efficientnet_pytorch import EfficientNet
+
+# model_path = '../PyTorch_Pretrained/resnet101-5d3b4d8f.pth'
+
+GlobalParams = collections.namedtuple('GlobalParams', [
+    'width_coefficient', 'depth_coefficient', 'image_size', 'dropout_rate',
+    'num_classes', 'batch_norm_momentum', 'batch_norm_epsilon',
+    'drop_connect_rate', 'depth_divisor', 'min_depth'])
+
+class EfficientNet(nn.Module):
+    def __init__(self, pretrained=True):
+        super(EfficientNet, self).__init__() 
+        self.seen = 0
+
+        self.res = EfficientNet.from_pretrained('efficientnet-b0') #perlu tambahin function from_pretrained?
+        
+        # self.convDU = convDU(in_out_channels=64,kernel_size=(1,9))
+        # self.convLR = convLR(in_out_channels=64,kernel_size=(9,1))
+
+        # Final linear layer
+        self._dropout = self._global_params.dropout_rate
+        self.output_layer = nn.Linear(out_channels, self._global_params.num_classes)
+
+        # import IPython; IPython.embed()
+
+    def forward(self,x):
+        x = self.res.extract_features(x)
+
+        # pdb.set_trace()
+
+        # x = self.convDU(x)
+        # x = self.convLR(x)
+        x = self.output_layer(x)
+
+        x = F.upsample(x,scale_factor=2)
+        return x
