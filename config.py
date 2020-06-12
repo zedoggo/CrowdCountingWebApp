528eda81aac83bfccc8d7913de1b5a1f69d1b773
Rivaldi Gunarso <gunarsorivaldi@gmail.com>
2020-06-12 17:04:20 +0700

    change max epoch to 100

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ __C.LR = 1e-5 # learning rate
 __C.LR_DECAY = 0.995 # decay rate
 __C.LR_DECAY_START = -1 # when training epoch is more than it, the learning rate will be begin to decay
 __C.NUM_EPOCH_LR_DECAY = 1 # decay frequency
-__C.MAX_EPOCH = 200
+__C.MAX_EPOCH = 100
 
 # multi-task learning weights, no use for single model, such as MCNN, VGG, VGG_DECODER, Res50, CSRNet, and so on
 
