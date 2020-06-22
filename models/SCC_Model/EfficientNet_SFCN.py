431ce141712a2bf0f442a9447552df3d58bdeb66
Rivaldi Gunarso <gunarsorivaldi@gmail.com>
2020-06-22 14:39:12 +0700

    efficientnet-b1 modified

--- models/SCC_Model/EfficientNet_SFCN.py
+++ models/SCC_Model/EfficientNet_SFCN.py
@@ -20,13 +20,13 @@ class EfficientNet_SFCN(nn.Module):
         super(EfficientNet_SFCN, self).__init__() 
         self.seen = 0
         
-        self.res = EfficientNet.from_pretrained('efficientnet-b7') #perlu tambahin function from_pretrained?
+        self.res = EfficientNet.from_pretrained('efficientnet-b1') #perlu tambahin function from_pretrained?
         
         self.frontend = nn.Sequential(
             self.res._conv_stem, self.res._bn0, self.res._swish
         )
 
-        self.convOut = nn.Sequential(nn.Conv2d(80, 64, kernel_size=1),nn.ReLU())
+        self.convOut = nn.Sequential(nn.Conv2d(40, 64, kernel_size=1),nn.ReLU())
         self.convDU = convDU(in_out_channels=64,kernel_size=(1,9))
         self.convLR = convLR(in_out_channels=64,kernel_size=(9,1))
 
@@ -39,14 +39,14 @@ class EfficientNet_SFCN(nn.Module):
         # x = self.res.extract_features(x)
         x = self.frontend(x)
 
-        for idx in range(18):            
+        for idx in range(8):            
             drop_connect_rate = self.res._global_params.drop_connect_rate
             if drop_connect_rate:
                 drop_connect_rate *= float(idx) / len(self.res._blocks) # scale drop connect_rate
             x = self.res._blocks[idx](x, drop_connect_rate=drop_connect_rate)
 
         # pdb.set_trace()
-        # import IPython; IPython.embed()
+        import IPython; IPython.embed()
 
         x = self.convOut(x)
         x = self.convDU(x)
